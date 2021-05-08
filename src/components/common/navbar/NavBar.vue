462beb29712d45d90e43d02565665701b2648abe
Abby <abby@AbbydeMacBook-Pro.local>
2021-05-08 10:40:56 +0800

    initia4

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <!--
+    div 的 class 是为了布局，占位
+    slot 的 name 属性是具名插槽，用来具体的替换
+  -->
+  <div class="nav-bar">
+    <div class="left">
+      <slot></slot>
+    </div>
+    <div class="center">
+      <slot></slot>
+    </div>
+    <div class="right">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+  /*<!-- 采用弹性布局-->*/
+  .nav-bar {
+    display: flex;
+    /*一般导航栏的高度为 44px */
+    height: 44px;
+    line-height: 44px;
+  }
+
+  .left, .right{
+    width: 60px;
+    background-color: #ff5777;
+  }
+
+  .center{
+    flex: 1;
+    background-color: #666666;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-      <h2>首页</h2>
+    <div id="home">
+<!--      首页里面只有中间部分有内容，所以就只替换 name 为 center的那个插槽-->
+      <nav-bar>
+        <div slot="center">
+          购物街
+        </div>
+      </nav-bar>
     </div>
 </template>
 
 <script>
+  import NavBar from "../../components/common/navbar/NavBar";
     export default {
-        name: "Home"
+        name: "Home",
+      components: {NavBar}
     }
 </script>
 
